8e7f7d086e1cd662e8280cacc9ae2a49a1512c57
shashwatrishi <shashwatrishi@gmail.com>
2024-07-29 16:57:35 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "example" {
     ami           = "ami-053b0d53c279acc90"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+
+resource "aws_s3_bucket" "lock_bucket"{
+    bucket_name = ""
+}
